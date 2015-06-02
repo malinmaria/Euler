49191b42c1b43c56f3f04f46d1fdf0ca3eb79e0e
Malin Heineking <mmheineking@gmail.com>
2015-06-02 10:28:39 -0400

    Adding each problem to Main

--- Main.java
+++ Main.java
@@ -1,12 +1,29 @@
 /**
- * Created by malinheineking on 5/18/15.
+ * Created by malinmaria on 5/18/15.
  */
 public class Main {
 
     public static void main(String args[]){
+
+        //Problem One
+        ProblemOne answerOne = new ProblemOne();
+        System.out.println(answerOne.multiples());
+
+        //Problem Three
+        ProblemThree answerThree = new ProblemThree();
+        System.out.println(answerThree.prime(600851475143L));
+
+        //Problem Five
+        ProblemFive answerFive = new ProblemFive();
+        answerFive.smallMult();
+
+        //Problem Six
         ProblemSix answer = new ProblemSix();
         answer.sumOfSquares();
         answer.squareOfSums();
-        System.out.println(answer.squareOfSums()-answer.sumOfSquares());
+        System.out.println(answer.squareOfSums()-answer.sumOfSquares()); //both functions must have a return type for this to work
+
+
+
     }
 }
